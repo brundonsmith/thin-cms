d659637cf79cbe49f3ebb8a21d8e38dcf59c217d
Brandon Smith <brandon.smith.945@gmail.com>
2016-08-07 14:14:18 -0500

    Bootstrapped

---
+++ app/main/main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/main/main.component.html',
+  styleUrls: ['app/main/main.component.css']
+})
+export class MainComponent { }
