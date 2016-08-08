b3490936bb4f9f11bdeeb56fe232a56de60faa6b
Brandon Smith <brandon.smith.945@gmail.com>
2016-08-07 22:51:27 -0500

    Added number input

---
+++ app/components/input-number/input-number.component.ts
@@ -0,0 +1,81 @@
+import { Component, HostBinding, HostListener, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+const noop = () => {
+};
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputNumberComponent),
+    multi: true
+};
+
+@Component({
+  moduleId: module.id,
+  selector: 'input-number',
+  templateUrl: 'input-number.component.html',
+  styleUrls: ['input-number.component.css'],
+  inputs: ['step', 'size'],
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
+})
+export class InputNumberComponent implements ControlValueAccessor {
+
+  public step: number = 1;
+  public size: number = 4;
+
+  onClickUp() {
+    this.value = this.value + +this.step;
+  }
+  onClickDown() {
+    this.value = this.value - this.step;
+  }
+
+  @HostListener('click', ['$event.target']) onClick(btn) {
+    //this.value = !this.value;
+  }
+
+
+  ////// To make ngModel work
+
+  //The internal data model
+  private innerValue: any = '';
+
+  //Placeholders for the callbacks which are later providesd
+  //by the Control Value Accessor
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  //get accessor
+  get value(): any {
+    return this.innerValue;
+  };
+
+  //set accessor including call the onchange callback
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  //Set touched on blur
+  onBlur() {
+    this.onTouchedCallback();
+  }
+
+  //From ControlValueAccessor interface
+  writeValue(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  //From ControlValueAccessor interface
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  //From ControlValueAccessor interface
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
+}
--- app/components/view-home/view-home.component.ts
+++ app/components/view-home/view-home.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { InputBooleanComponent } from '../input-boolean/input-boolean.component';
+import { InputNumberComponent } from '../input-number/input-number.component';
 
 @Component({
   moduleId: module.id,
   selector: 'view-home',
   templateUrl: 'view-home.component.html',
   styleUrls: ['view-home.component.css'],
-  directives: [InputBooleanComponent]
+  directives: [InputBooleanComponent, InputNumberComponent]
 })
 export class ViewHomeComponent {
 
   public testBoolean: boolean = true;
+
+  public testNumber: number = 12;
 
 }
